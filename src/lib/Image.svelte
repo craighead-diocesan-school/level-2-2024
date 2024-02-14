d63a4e4d4121135c58b03a05a2d64532c00b1255
Username-ray <maedara96@craighead.school.nz>
2024-02-14 18:47:21 +1300

    Changed image size

--- src/lib/Image.svelte
+++ src/lib/Image.svelte
@@ -4,7 +4,7 @@
   export let caption = "Sea and sand"
 </script>
 
-<figure class="image is-256x128">
+<figure class="image is-square">
   <img {src} {alt} />
   <figcaption>{caption}</figcaption>
 </figure>
